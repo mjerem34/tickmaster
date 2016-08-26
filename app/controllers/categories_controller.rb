487769d90dbf4d071b6105f1fae1a6d73ba9afc9
CLE <jeremy.montesinos@cle-expertises.fr>
2016-08-26 10:49:26 +0200

    Commentaires CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,9 @@ class CategoriesController < ApplicationController
 
   # GET /categories
   # GET /categories.json
+  # Should get and return all the categories.
+  # If html, it render the index page.
+  # If rest, it render json with all categories details.
   def index
     @view_index_categories = verifRight('view_index_categories')
     if @view_index_categories
@@ -12,7 +15,7 @@ class CategoriesController < ApplicationController
       @delete_category = verifRight('delete_category')
       @categories = Category.all
       @category = Category.new
-      @title = "Catégories"
+      @title = 'Catégories'
       respond_to do |format|
         format.json { render json: @categories }
         format.html { render :index }
@@ -24,9 +27,12 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1
   # GET /categories/1.json
+  # Should render one category.
+  # If html, it render the edit category page, where there are the subcategories
+  # listed by name.
   def show
     if verifRight('view_details_category')
-      @title = "Catégorie : " + @category.name
+      @title = 'Catégorie : ' + @category.name
       respond_to do |format|
         format.json { render json: @category }
         format.html { redirect_to :edit_category }
@@ -37,10 +43,11 @@ class CategoriesController < ApplicationController
   end
 
   # GET /categories/new
+  # Should render the new form for create an category, only for web.
   def new
     @create_new_category = verifRight('create_new_category')
     if @create_new_category
-      @title = "Nouvelle catégorie"
+      @title = 'Nouvelle catégorie'
       @category = Category.new
     else
       renderUnauthorized
@@ -48,6 +55,7 @@ class CategoriesController < ApplicationController
   end
 
   # GET /categories/1/edit
+  # Should render the edit form for edit an category, only for web.
   def edit
     @edit_category = verifRight('edit_category')
     if @edit_category
@@ -55,12 +63,13 @@ class CategoriesController < ApplicationController
       @sous_category = SousCategory.new
     else
       renderUnauthorized
-  end
+    end
   end
 
+  # TODO: We can delete this mthd and do the thing with the mthd create.
   def create_cats
     if verifRight('create_new_category')
-      @title = "Nouvelle catégorie"
+      @title = 'Nouvelle catégorie'
       @category = Category.new(name: params[:category_name])
       respond_to do |format|
         if @category.save
@@ -76,19 +85,22 @@ class CategoriesController < ApplicationController
 
   # POST /categories
   # POST /categories.json
+  # Should do the job for create the category.
+  # The mthd create also an subcategory called 'Autre' because we need it.
   def create
     if verifRight('create_new_category')
-      @title = "Nouvelle catégorie"
+      @title = 'Nouvelle catégorie'
       @category = Category.new(category_params)
       respond_to do |format|
         if @category.save
           format.json { render json: @category, status: :created }
           format.html { redirect_to :back, notice: 'Catégorie crée avec success.' }
+          # Create an new subcategory for the category created.
           @sous_category = SousCategory.new(name: 'Autre', category_id: @category.id, lvl_urgence_max: '10')
           @sous_category.save
         else
           format.json { render json: @category.errors, status: :unprocessable_entity }
-          format.html { redirect_to :back, notice: "Impossible de créer la catégorie ..." }
+          format.html { redirect_to :back, notice: 'Impossible de créer la catégorie ...' }
         end
       end
     else
@@ -98,15 +110,16 @@ class CategoriesController < ApplicationController
 
   # PATCH/PUT /categories/1
   # PATCH/PUT /categories/1.json
+  # Should update the params of the category selected (passed in params).
   def update
     if verifRight('edit_category')
       respond_to do |format|
         if @category.update(category_params)
           format.json { render json: @category, status: :ok }
-          format.html { redirect_to :back, notice: "Catégorie actualisée avec success." }
+          format.html { redirect_to :back, notice: 'Catégorie actualisée avec success.' }
         else
           format.json { render json: @category.errors, status: :unprocessable_entity }
-          format.html { render :back, notice: "Impossible de modifier la catégorie ..." }
+          format.html { render :back, notice: 'Impossible de modifier la catégorie ...' }
         end
       end
     else
@@ -116,21 +129,23 @@ class CategoriesController < ApplicationController
 
   # DELETE /categories/1
   # DELETE /categories/1.json
+  # Should destroy the category selected (passed in params).
+  # But only if it does not contains incidents.
+  # Because if not, an thermo-nuclear war begin...
   def destroy
     if verifRight('delete_category')
       if Incident.where(category_id: @category.id).pluck(:title).join('') == ''
         @category.destroy!
         respond_to do |format|
           format.json { head :no_content }
-          format.html { redirect_to categories_url, notice: "Catégorie supprimée" }
+          format.html { redirect_to categories_url, notice: 'Catégorie supprimée' }
         end
       else
         respond_to do |format|
-          format.json { render json: "Impossible de supprimer la catégorie car elle contient des incidents !", status: 409 }
-          format.html { redirect_to categories_url, notice: "Vous ne pouvez pas supprimer cette catégorie car elle contient des incidents !" }
+          format.json { render json: 'Impossible de supprimer la catégorie car elle contient des incidents !', status: 409 }
+          format.html { redirect_to categories_url, notice: 'Vous ne pouvez pas supprimer cette catégorie car elle contient des incidents !' }
         end
       end
-
     else
       renderUnauthorized
     end
