1ddd80c21a7adb94ee19021698f929be2be6fb08
mjerem34 <mjerem34@opmbx.org>
2017-03-20 16:51:01 +0100

    Modification dans incidents_state, ajout des couleurs pour les labels

--- db/migrate/3_create_incidents_states.rb
+++ db/migrate/3_create_incidents_states.rb
@@ -3,6 +3,8 @@ class CreateIncidentsStates < ActiveRecord::Migration
     create_table :incidents_states do |t|
       t.string :name, null: false, unique: true
       t.string :shortcut, null: false, unique: true
+      t.string :user_color, null: false, unique: true
+      t.string :tech_color, null: false, unique: true
     end
   end
 end
